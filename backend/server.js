f7b3ab2fde192822855760f4436bcd25780cbdb8
ibrahimyasser450 <ibrahimyasser450@gmail.com>
2026-08-16 19:54:57 +0300

    last update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,13 @@ connectDB();
 connectCloudinary();
 // middlewares
 app.use(express.json());
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  process.env.FRONTEND_URL,
+  process.env.ADMIN_URL,
+  ,
+];
 
 app.use(
   cors({
